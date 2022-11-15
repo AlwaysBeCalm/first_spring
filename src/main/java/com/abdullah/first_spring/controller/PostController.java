eda10e319693ce961307f843f4924f652c806979
Abdullah Nasser <abdullah-nasser-@outlook.com>
2022-11-15 16:50:15 +0300

    added method to handle adding new post

--- src/main/java/com/abdullah/first_spring/controller/PostController.java
+++ src/main/java/com/abdullah/first_spring/controller/PostController.java
@@ -18,9 +18,20 @@ public class PostController {
 
     // create handler method for GET request and return model and view
     @GetMapping("/admin/posts")
-    public String posts(Model model) {
+    public String posts(Model model) { // try to remove the model param
         List<PostDto> posts = postService.findAllPosts();
         model.addAttribute("posts", posts);
         return "/admin/posts";
+    }
+
+//    @RequestMapping(
+//            name = "/admin/new-post",
+//            method = {RequestMethod.GET, RequestMethod.POST}
+//    )
+    @GetMapping("/admin/posts/new-post")
+    public String newPostForm(Model model) {
+        PostDto postDto = new PostDto();
+        model.addAttribute("postDto", postDto);
+        return "admin/new-post";
     }
 }
