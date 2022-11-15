6dcafc33a398f991d8d4a13d9c85ef71669f7f26
Abdullah Nasser <abdullah-nasser-@outlook.com>
2022-11-15 16:50:43 +0300

    added annotation for no args and all args constructors

--- src/main/java/com/abdullah/first_spring/dto/PostDto.java
+++ src/main/java/com/abdullah/first_spring/dto/PostDto.java
@@ -1,12 +1,16 @@
 package com.abdullah.first_spring.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostDto {
     // Dto = Data Transform Object
     private Long id;
